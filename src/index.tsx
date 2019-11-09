37b958536794ba0d952aaca62a7083c454b9d20c
bszhct <camel@bszhct.com>
2019-11-09 17:22:09 +0800

    up

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ export default class App extends React.Component {
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // 在后台运行的一个线程处理离线缓存, 消息推送, 后台自动更新等任务
-// 下次访问时，就可以更快的获取资源
+// 下次访问时, 就可以更快的获取资源
 // 离线使用
 // 只在生产环境中生效
 serviceWorker.unregister()
